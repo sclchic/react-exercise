9a2932fb848375f1602b06fee7ab0d4adfcf995b
姗姗来迟 <sunchengli@weiyankeji.cn>
2019-05-17 12:52:55 +0800

    Updates

---
+++ precache-manifest.9a07b71ed0de4910de56060688093846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8381d28b342c1d3d182c15cdf460a67d",
+    "url": "/ChicFE/index.html"
+  },
+  {
+    "revision": "fc841181b8c2a66a2825",
+    "url": "/ChicFE/static/css/main.fe22d581.chunk.css"
+  },
+  {
+    "revision": "20d1bb46ff6c583b9b3b",
+    "url": "/ChicFE/static/js/2.5fd1c813.chunk.js"
+  },
+  {
+    "revision": "fc841181b8c2a66a2825",
+    "url": "/ChicFE/static/js/main.0f4fdb9a.chunk.js"
+  },
+  {
+    "revision": "6b18bf9aa5f8f5a0fcb0",
+    "url": "/ChicFE/static/js/runtime~main.d60ca703.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.789e1e17c6685cd1feeb57229a297b4d.js"
+  "/ChicFE/precache-manifest.9a07b71ed0de4910de56060688093846.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChicFE/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
