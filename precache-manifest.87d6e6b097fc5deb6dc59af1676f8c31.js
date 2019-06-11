b7bbf08c46e645da16f15fb4a5462c83a886bd19
姗姗来迟 <sunchengli@weiyankeji.cn>
2019-06-11 14:06:57 +0800

    Updates

---
+++ precache-manifest.87d6e6b097fc5deb6dc59af1676f8c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff300424471c50285f98d3e4b4b5601",
+    "url": "/react-exercise/index.html"
+  },
+  {
+    "revision": "020cd984abb329b6678f",
+    "url": "/react-exercise/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "ca213c523fdcb24bd769",
+    "url": "/react-exercise/static/css/main.fe22d581.chunk.css"
+  },
+  {
+    "revision": "020cd984abb329b6678f",
+    "url": "/react-exercise/static/js/2.c0d40285.chunk.js"
+  },
+  {
+    "revision": "ca213c523fdcb24bd769",
+    "url": "/react-exercise/static/js/main.5d1153cb.chunk.js"
+  },
+  {
+    "revision": "de81669b0a7d9eca5e4e",
+    "url": "/react-exercise/static/js/runtime~main.9a06d9ab.js"
+  }
+]);
